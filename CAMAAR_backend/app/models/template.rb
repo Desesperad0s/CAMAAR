f5ec5686b8d59ef13a890c964bf6dec6b7f7bdd3
EmersonJr <emersonjr02@gmail.com>
2025-07-10 18:50:52 -0300

    merging

--- CAMAAR_backend/app/models/template.rb
+++ CAMAAR_backend/app/models/template.rb
@@ -1,5 +1,5 @@
 class Template < ApplicationRecord
-  belongs_to :admin
+  belongs_to :admin, foreign_key: :admin_id
   has_many :questoes
   has_many :formularios
 end
--- CAMAAR_backend/app/models/turma_aluno.rb
+++ CAMAAR_backend/app/models/turma_aluno.rb
@@ -1,4 +1,4 @@
 class TurmaAluno < ApplicationRecord
-  belongs_to :turma
-  belongs_to :user
+  belongs_to :turma, foreign_key: :turma_id
+  belongs_to :user, foreign_key: :user_id
 end
