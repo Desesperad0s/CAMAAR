b79b7aeb1058c6fbeb7501082960dbb56a73a781
Lucas Lima <lucasgabrieljake@gmail.com>
2025-07-09 21:59:01 -0300

    feat: user module

---
+++ CAMAAR_backend/db/migrate/20250710005829_create_turmas.rb
@@ -0,0 +1,12 @@
+class CreateTurmas < ActiveRecord::Migration[8.0]
+  def change
+    create_table :turmas do |t|
+      t.string :code
+      t.integer :number
+      t.string :semester
+      t.string :time
+
+      t.timestamps
+    end
+  end
+end
