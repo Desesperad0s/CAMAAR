296967df6485d6c57377b1f69d4c283de3cd71fc
Lucas Lima <lucasgabrieljake@gmail.com>
2025-07-11 00:12:29 -0300

    chore: cors configurations

--- CAMAAR_backend/config/initializers/content_security_policy.rb
+++ CAMAAR_backend/config/initializers/content_security_policy.rb
@@ -4,22 +4,28 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    # Permite todas as fontes para uma API que será acessada por diferentes origens
+    policy.default_src :self, :https, '*'
+    policy.font_src    :self, :https, :data, '*'
+    policy.img_src     :self, :https, :data, '*'
+    policy.object_src  :none
+    policy.script_src  :self, :https, '*'
+    policy.style_src   :self, :https, '*'
+    
+    # Permitir conexões de qualquer origem
+    policy.connect_src :self, :https, '*'
+    
+    # Você pode adicionar um endpoint para relatórios de violação
+    # policy.report_uri "/csp-violation-report-endpoint"
+  end
+
+  # Não aplicar a política em modo estrito para API
+  # config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  # config.content_security_policy_nonce_directives = %w(script-src style-src)
+
+  # Apenas relatar violações sem forçar a política
+  config.content_security_policy_report_only = true
+end
