7ffa04bd2ccab7711513ff16faa585f64345d8e1
Lucas Lima <lucasgabrieljake@gmail.com>
2025-07-11 00:08:19 -0300

    feat: users module

--- CAMAAR_backend/app/models/user.rb
+++ CAMAAR_backend/app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_many :turmas_alunos
+
+  validates :registration, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :role, presence: true, inclusion: { in: ['student', 'professor', 'admin'] }
+  
+  def estudante?
+    role == 'student'
+  end
+  
+  def professor?
+    role == 'professor'
+  end
 end
