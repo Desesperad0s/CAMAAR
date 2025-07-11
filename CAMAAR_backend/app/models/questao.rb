11bc356a0dcf93b971e0ae73f3a153853d3268c4
Lucas Lima <lucasgabrieljake@gmail.com>
2025-07-11 18:18:22 -0300

    feat: alternativas module

--- CAMAAR_backend/app/models/questao.rb
+++ CAMAAR_backend/app/models/questao.rb
@@ -3,8 +3,12 @@ class Questao < ApplicationRecord
   
   belongs_to :template, foreign_key: :templates_id, inverse_of: :questoes
   belongs_to :formulario, foreign_key: :formularios_id, optional: true
-  has_many :alternativas, foreign_key: :questao_id
+  has_many :alternativas, foreign_key: :questao_id, dependent: :destroy
   has_many :respostas, class_name: 'Resposta', foreign_key: :questao_id
   
   validates :enunciado, presence: true
+  
+  accepts_nested_attributes_for :alternativas, 
+                               allow_destroy: true,
+                               reject_if: :all_blank
 end
