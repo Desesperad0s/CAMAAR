8c259fafcb3530e682bcf06e607740c7cb3465a1
Pedro Neris <pedrolucaspn13@gmail.com>
2025-07-13 00:08:05 -0300

    submitting forms

--- CAMAAR_frontend/src/App.js
+++ CAMAAR_frontend/src/App.js
@@ -4,6 +4,8 @@ import Login from './pages/Login';
 import Resultados from './pages/Resultados';
 import EnviarFormulario from './pages/EnviarFormulario';
 import Templates from './pages/Templates';
+import AnswerForm from './pages/AnswerForm';
+import AdminCreateForm from './pages/AdminCreateForm';
 import './App.css';
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/enviar" element={<EnviarFormulario />} />
         <Route path="/templates" element={<Templates />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/answer-form" element={<AnswerForm />} />
+        <Route path="/admin/create-form" element={<AdminCreateForm />} />
       </Routes>
     </Router>
   );
--- CAMAAR_frontend/src/utils/apiClient.ts
+++ CAMAAR_frontend/src/utils/apiClient.ts
@@ -5,12 +5,58 @@ export class Api {
     api;
     constructor() {
         this.api = new HttpClient(
-            BACK_URL, 
+            BACK_URL,
             { credentials: 'include' }
         );
     }
 
     async login(email, password) {
-        return await this.api.post(`/login`, { email, password });    
+        return await this.api.post(`/login`, { email, password });
+    }
+
+    async getFormulario(id) {
+        return await this.api.get(`/formularios/${id}`);
+    }
+
+    async createFormularioWithTemplate(templateId, turmaId, name, date) {
+        return await this.api.post('/formularios/create_with_questions', {
+            template_id: templateId,
+            turma_id: turmaId,
+            name: name,
+            date: date
+        });
+    }
+
+
+    async getTemplate(id) {
+        return await this.api.get(`/templates/${id}`);
+    }
+
+    async getTurmas() {
+        return await this.api.get('/turmas');
+    }
+
+    async getTurma(id) {
+        return await this.api.get(`/turmas/${id}`);
+    }
+
+    async getDisciplinas() {
+        return await this.api.get('/disciplinas');
+    }
+
+    async getDisciplina(id) {
+        return await this.api.get(`/disciplinas/${id}`);
+    }
+
+    async getQuestoes(formularioId) {
+        return await this.api.get(`/questaos?formulario_id=${formularioId}`);
+    }
+
+    async getAlternativas(questaoId) {
+        return await this.api.get(`/alternativas?questao_id=${questaoId}`);
+    }
+
+    async getAdmins() {
+        return await this.api.get('/admins');
     }
 }
