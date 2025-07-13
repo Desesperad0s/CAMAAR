801c64989193f81c8a30ed7ca27ebb67155db8ea
EMERSON LUIZ CRUZ JUNIOR <emersonjr02@gmail.com>
2025-07-13 11:11:34 -0300

    fix: fixing mistakes

--- CAMAAR_backend/db/schema.rb
+++ CAMAAR_backend/db/schema.rb
@@ -39,7 +39,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_13_030000) do
   create_table "disciplinas", force: :cascade do |t|
     t.string "name"
     t.string "code"
-    t.integer "departamento_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "departamento_id"
