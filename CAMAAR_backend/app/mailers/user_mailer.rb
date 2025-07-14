fff32918ed8d84578e272dabc4bdd19e3258b60f
EMERSON LUIZ CRUZ JUNIOR <emersonjr02@gmail.com>
2025-07-13 21:36:56 -0300

    feat: now is possible to send emails

--- CAMAAR_backend/app/mailers/user_mailer.rb
+++ CAMAAR_backend/app/mailers/user_mailer.rb
@@ -1,9 +1,14 @@
-class UserMailer < ApplicationMailer
-  def send_password_email
-    @user = params[:user]
+class UserMailer < Devise::Mailer
+  helper :application # Inclui helpers padrão
+  include Devise::Controllers::UrlHelpers # Inclui helpers do Devise
+  default template_path: 'devise/mailer' # Usa os templates padrão do Devise
+
+  def send_set_password_email(user)
+    @user = user
+    @set_password_url = edit_user_password_url(@user, reset_password_token: @user.send(:set_reset_password_token))
     mail(
       to: @user.email,
-      subject: 'Bem-vindo ao sistema CAMAAR!'
+      subject: 'Configure sua senha para acessar o sistema CAMAAR'
     )
   end
 end
---
+++ CAMAAR_backend/config/initializers/devise.rb
@@ -0,0 +1,4 @@
+Devise.setup do |config|
+  # Configurar o mailer personalizado
+  config.mailer = 'UserMailer'
+end
